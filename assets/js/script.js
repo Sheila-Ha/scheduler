40d10c46cf9c46c7fa92038b4d1201906d047bb9
sheilahanson <slhanson11@live.com>
2023-11-29 10:52:28 -0600

    wrap code that interacts with DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").wrapAll("<div></div>");
+    }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage. 
+  // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +26,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+});       
+
+const currentDate = dayjs(); const formattedDate = currentDate. format('YYYY-MM-DD HH:mm:ss'); 
+dayjs().format('MMM D,YYYY');});
