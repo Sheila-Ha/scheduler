c9afe9392a797b107a9dd76f8cb5155434b94791
sheilahanson <slhanson11@live.com>
2023-12-05 20:06:04 -0600

    moved img to inside assests, moved var appointments to top and added ||[]

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
+  var appointments = JSON.parse(localStorage.getItem('appointments'))||[];
   $(document).ready(function(){
-    var appointments = JSON.parse(localStorage.getItem('appointments'));
     $(appointments).each(function(index) {
       //console.log(this.text + ' ' + this.time);
       //on page load, app finds any appointments stored in local storage
